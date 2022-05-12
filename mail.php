56a6935d80e4de0d51a22b57fe24e26380f3e38f
Kush Panthi <73062142+kushpanthi@users.noreply.github.com>
2022-05-12 22:15:04 +0545

    Update mail.php

--- mail.php
+++ mail.php
@@ -10,14 +10,10 @@ $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Phone Nuumber =" . $n
 "\r\n Message =" . $message;
 
 $headers = "From: noreply@kushpanthi.com.np" . "\r\n";
-$mail = mail($to,$subject,$message,$headers);
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
 
-if ($mail){
-    echo "<script>alert('Mail Send.');</script>";
-}else{
-    echo "<script>alert('Mail Not Send.');</script>";
-}
+//redirect
+header("Location:thankyou.html)
 ?>
